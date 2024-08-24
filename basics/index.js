9700af7fdac7bf8d1fbe195fc452653d87a3ea9d
NarsiBhatii <narsibhati31102@gmail.com>
2024-08-24 21:00:51 +0530

    basic Day 1 node js

---
+++ basics/index.js
@@ -0,0 +1,2 @@
+let fullName = "Narsi Bhati";
+console.log(fullName);
---
+++ basics/index.mjs
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello, world!"));
+console.log(chalk.red.bold("This is an error message."));
+console.log(chalk.green.underline("This is a success message."));
